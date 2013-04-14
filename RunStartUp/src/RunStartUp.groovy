c5cdd7a173faf72c84c2f3105f54cb88144bb5fd
Avnit Bambah <abambah@gmail.com>
2013-04-14 11:09:54 -0500

    Update RunStartUp.groovy
    Making it a json object so i can pass it to the json parser in next step .

--- RunStartUp/src/RunStartUp.groovy
+++ RunStartUp/src/RunStartUp.groovy
@@ -1,12 +1,13 @@
 
-
 import javax.swing.ImageIcon 
 import java.awt.BorderLayout as BL
 import groovy.swing.SwingBuilder 
 
 class RunStartUp {
 /*
- * testing if i can do a multiplication table in Java with no issues. 
+ * testing if i can get the google api to get the json object 
+ * next step is take this json object and call the parser class on this object.
+ * finally make it a webservice and call it in my webpage . 
  */
     static main(args) {
         
@@ -15,9 +16,10 @@ class RunStartUp {
                  q:'Text to ',
                  z:'case'];
     
-    println base.toURL().text;
+    //println base.toURL().text;
+    def jsonObject = base.toURL().text;
     
-    
+    println jsonObject;
    
     }
 
