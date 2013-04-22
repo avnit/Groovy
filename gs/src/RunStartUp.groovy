40e6f4420ffd9545407ef9c3be25a551fbebd3d8
avnit bamabah <abambah@gmail.com>
2013-04-21 22:02:04 -0500

    args

--- gs/src/RunStartUp.groovy
+++ gs/src/RunStartUp.groovy
@@ -4,50 +4,58 @@ import groovy.swing.SwingBuilder
 import groovy.json.JsonSlurper
 
 class RunStartUp {
-	/*
-	 * testing if i can get the google api to get the json object 
-	 * next step is take this json object and call the parser class on this object.
-	 * finally make it a webservice and call it in my webpage . 
-	 */
-	 class params {
-	 	String name , 
-	 	String value
-	 }
-	static main(String[] args) {
+    /*
+     * testing if i can get the google api to get the json object 
+     * next step is take this json object and call the parser class on this object.
+     * finally make it a webservice and call it in my webpage . 
+     */
+     
+         String name 
+         String value
+     
+    static main(String[] args) {
            
-		def base = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=avnit";
-		// def base = "https://ajax.googleapis.com/ajax/sevices/search/web?";
-		def param = [
-			new params (name: "v" ,value :'1.0'),
-			new params (name: "q" , value :'Avnit%20Bambah')
-			// new params (name:"q" , value : args[0]) 
-			// need to get search variable from command line 
-			];
-		def param_string = '';
-			param.each{
-				param_string = "$param.name=$param.value&";
-			}
-		def finalurl = "$base$param_string";
-		print finalurl;
-		base.plus(param);
-		print base.plus(param);
+        def base = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=avnit";
+        // def base = "https://ajax.googleapis.com/ajax/sevices/search/web?";
+        def param = [
+            new RunStartUp (name: "v" ,value :'1.0'),
+         //   new RunStartUp (name: "q" , value :'Avnit%20Bambah'),
+			new RunStartUp (name: "q" , value :args[0]),
+        //    new params (name:"q" , value : args[0]) 
+            // need to get search variable from command line 
+            ];
+        def param_string = '';
+            param.each{
+                param_string = "$param.name=$param.value&";
+            }
+        def finalurl = "$base$param_string";
+        println finalurl;
+        base.plus(param);
+        println base.plus(param);
 
-		//println base.toURL().text;
-		def jsonObject = base.toURL().text;
+        //println base.toURL().text;
+        def jsonObject = base.toURL().text;
 
-		def slurper = new JsonSlurper()
-		def result = slurper.parseText(jsonObject);
+        def slurper = new JsonSlurper()
+        def result = slurper.parseText(jsonObject);
 
-		println result.toString();
-		
-		def urls = result.responseData.results.url;
-		for (int i=0 ; i < 5 ; i++) {
-			println urls[i];
-		}
+        println result.toString();
+        
+        def urls = result.responseData.results.url;
+       def content =  result.responseData.results.content
+	   def j =0;
+	    result.responseData.results.content.each { 
+			println content[j] 
+			j++;
+			};
+        for (int i=0 ; i < 5 ; i++) {
+            println urls[i] ;
+            println content[i];
+        }
         
 
-		// println result.responseData.results.url;
+         
 
-	}
+    }
 
 }
